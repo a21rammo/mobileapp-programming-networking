b3e2072d6ee381cffc370914059426882b2c9f06
Raman Mohammed <a21rammo@outlook.com>
2022-04-25 19:41:58 +0200

    Added mountain and myadapter classes, really unsure where to get the json file from.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -5,11 +5,20 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
+    private final ArrayList<Mountain> mountainArrayList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,11 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
 
         new JsonFile(this, this).execute(JSON_FILE);
+
+        Gson gson = new Gson();
+        Type type =  new TypeToken<List<Mountain>>() {}.getType();
+        List<Mountain> mountainList = gson.fromJson(JSON_FILE, type);
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,26 @@
+package com.example.networking;
+
+public class Mountain {
+    private String name;
+    private String location;
+    private int height;
+
+    public Mountain(String mName, String mLocation, int mHeight) {
+        this.name= mName;
+        this.location= mLocation;
+        this.height = mHeight;
+    }
+
+    public void setName(String mName) {
+        name=mName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
