ed798dd99ddf7882518d49215970bcb5b92bfea8
Raman Mohammed <a21rammo@hotmail.com>
2022-04-26 11:13:17 +0200

    Added methods in Adapter

--- app/src/main/java/com/example/networking/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/networking/RecyclerViewAdapter.java
@@ -1,5 +1,33 @@
 package com.example.networking;
 
-public class RecyclerViewAdapter extends Mountain.adapter<RecyclerViewAdapter> {
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    public RecyclerViewAdapter() {
+
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder {
+
+    }
 }
