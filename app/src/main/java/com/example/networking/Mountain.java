ed6b88341162fbdb134324e4d6688db1fc8ec0bc
Raman Mohammed <a21rammo@outlook.com>
2022-04-28 15:39:34 +0200

    Fixed mountain class and xml to showcase right data

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,16 +1,41 @@
 package com.example.networking;
 
-public class Mountain {
+public class Mountain{
     private String name;
+    private String location;
+    private String type;
+    private int size;
+    private int cost;
 
-    public Mountain(String name) {
-        this.name = name;
+
+    public Mountain(String mName, String mLocation, String mType) {
+        this.name= mName;
+        this.location= mLocation;
+        this.type = mType;
     }
 
     public String getName() {
         return name;
     }
 
-    public class adapter<T> {
+    public void setName(String name) {
+        this.name = name;
     }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
 }
---
+++ app/src/main/java/com/example/networking/MyAdapter.java
@@ -0,0 +1,32 @@
+package com.example.networking;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    public MyAdapter() {
+
+    }
+
+    @NonNull
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
